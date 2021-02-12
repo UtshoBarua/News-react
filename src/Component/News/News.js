bb1d632f59f775330ec6ad92514ef5c61cefd2fc
UtshoBarua <utsho45-211@diu.edu.bd>
2021-02-13 00:18:25 +0600

    news-react

---
+++ src/Component/News/News.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Card,Button} from 'react-bootstrap'
+const News = (props) => {
+ 
+    return (
+        <Card style={{ width: '18rem' }}>
+        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Body>
+    <Card.Title></Card.Title>
+          <Card.Text>
+            
+          </Card.Text>
+          <Button variant="primary">Go somewhere</Button>
+        </Card.Body>
+      </Card>
+    );
+};
+
+export default News;
---
+++ src/Component/TopHeadline/TopHeadline.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from 'react';
+import News from '../News/News';
+
+const TopHeadline = () => {
+    const [articles,setArticles] = useState([])
+    
+    useEffect(()=>{
+        const url ='http://newsapi.org/v2/everything?q=tesla&from=2021-01-12&sortBy=publishedAt&apiKey=0487ef6c448d44619dd07b5283d433ff '
+        fetch(url)
+        .then(res=>res.json())
+        .then(data=> console.log(data.articles))
+        
+       
+    },[])
+    return (
+        <div>
+            <h1> the result is {articles.length} </h1>
+            {
+                articles.map(article => <News article={article} ></News>)
+            }
+        </div>
+    );
+};
+
+export default TopHeadline;
