2eb700cb8b70cc9ed181561eac052d8a5cedb52f
UtshoBarua <utsho45-211@diu.edu.bd>
2021-02-13 20:23:27 +0600

    news

--- src/Component/News/News.js
+++ src/Component/News/News.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Card,Button} from 'react-bootstrap'
 const News = (props) => {
- 
+     console.log(props.article);
     return (
         <Card style={{ width: '18rem' }}>
         <Card.Img variant="top" src="holder.js/100px180" />
--- src/Component/TopHeadline/TopHeadline.js
+++ src/Component/TopHeadline/TopHeadline.js
@@ -5,19 +5,20 @@ const TopHeadline = () => {
     const [articles,setArticles] = useState([])
     
     useEffect(()=>{
-        const url ='http://newsapi.org/v2/everything?q=tesla&from=2021-01-12&sortBy=publishedAt&apiKey=0487ef6c448d44619dd07b5283d433ff '
+        const url ='http://newsapi.org/v2/everything?q=tesla&from=2021-01-13&sortBy=publishedAt&apiKey=0487ef6c448d44619dd07b5283d433ff'
         fetch(url)
         .then(res=>res.json())
-        .then(data=> console.log(data.articles))
+        .then(data=> setArticles(data.articles))
         
+
        
     },[])
     return (
         <div>
-            <h1> the result is {articles.length} </h1>
-            {
-                articles.map(article => <News article={article} ></News>)
-            }
+            <h1> the result is{articles.length} </h1>
+           {
+               articles.map(article=><News article={article} ></News> )
+           }
         </div>
     );
 };
